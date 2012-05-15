09f5069dde83a006486b4b02869ee163f737c773
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-15 13:12:00 +0000

    Disable image sprite
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@35997 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/resources/GlobalResources.java
+++ metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/resources/GlobalResources.java
@@ -3,47 +3,62 @@ package org.siemac.metamac.statistical.operations.web.client.resources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
+import com.google.gwt.resources.client.ImageResource.RepeatStyle;
 
 public interface GlobalResources extends ClientBundle {
 
     public static final GlobalResources RESOURCE = GWT.create(GlobalResources.class);
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/istacLogo.gif")
     ImageResource istacLogo();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/new_listgrid.png")
     ImageResource newListGrid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/edit_listgrid.png")
     ImageResource editListGrid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/save_listgrid.png")
     ImageResource saveListGrid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/delete_listgrid.png")
     ImageResource deleteListGrid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/cancel_listgrid.png")
     ImageResource cancelListGrid();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/info.png")
     ImageResource info();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/error.png")
     ImageResource error();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/ask.png")
     ImageResource ask();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/success.png")
     ImageResource success();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/publish_internally.png")
     ImageResource publishInternally();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/publish_externally.png")
     ImageResource publishExternally();
 
+    @ImageOptions(repeatStyle = RepeatStyle.Both)
     @Source("images/unpublish_externally.png")
     ImageResource unpublishExternally();
 
