269d69ee89f9665ebad10dc7c7b92cf90085534c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-23 11:31:18 +0000

    Entidad InternationalString
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@36572 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-operations-core/src/main/java/org/siemac/metamac/core/common/ent/domain/InternationalString.java
@@ -0,0 +1,30 @@
+package org.siemac.metamac.core.common.ent.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * The International String is a collection of Localised Strings and supports the representation of text in multiple locales.
+ */
+@Entity
+@Table(name = "TB_INTERNATIONAL_STRINGS")
+public class InternationalString extends InternationalStringBase {
+
+    private static final long serialVersionUID = 1L;
+
+    public InternationalString() {
+    }
+
+    public String getLocalisedLabel(String locale) {
+        if (locale == null) {
+            return null;
+        }
+        for (LocalisedString localstr : getTexts()) {
+            if (locale.equalsIgnoreCase(localstr.getLocale())) {
+                return localstr.getLabel();
+            }
+        }
+        return null;
+    }
+
+}
