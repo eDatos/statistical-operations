1c108f54cdebf737ace93f33765c7393c7aed9f6
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-25 09:17:57 +0000

    MOCK en serviceContext
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@36753 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/server/ServiceContextHolder.java
+++ metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/server/ServiceContextHolder.java
@@ -12,12 +12,13 @@ public class ServiceContextHolder {
     private static String SESSION_ATTRIBUTE = "_serviceContext_";
 
     public static ServiceContext getCurrentServiceContext() {
-        ServiceContext serviceContext = (ServiceContext) getCurrentRequest().getSession().getAttribute(ServiceContextHolder.SESSION_ATTRIBUTE);
-        if (serviceContext == null) {
-            serviceContext = new ServiceContext("ANNONIMOUS", "NO_SESSION", StatisticalOperationsConstants.SECURITY_APPLICATION_ID); // TODO
-            putCurrentServiceContext(serviceContext);
-        }
-        return serviceContext;
+        // TODO
+        // ServiceContext serviceContext = (ServiceContext) getCurrentRequest().getSession().getAttribute(ServiceContextHolder.SESSION_ATTRIBUTE);
+        // if (serviceContext == null) {
+        // serviceContext = new ServiceContext("ANNONIMOUS", "NO_SESSION", SrmConstants.SECURITY_APPLICATION_ID);
+        // putCurrentServiceContext(serviceContext);
+        // }
+        return new ServiceContext("ANNONIMOUS", "NO_SESSION", StatisticalOperationsConstants.SECURITY_APPLICATION_ID);
     }
 
     public static void putCurrentServiceContext(ServiceContext serviceContext) {
