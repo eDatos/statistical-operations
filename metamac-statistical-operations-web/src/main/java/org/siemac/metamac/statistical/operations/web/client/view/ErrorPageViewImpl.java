2faf459f97b5fcfe454291de88e9dc005743ee3b
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-15 07:20:00 +0000

    ErrorPage
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@37713 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/view/ErrorPageViewImpl.java
+++ metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/view/ErrorPageViewImpl.java
@@ -2,10 +2,13 @@ package org.siemac.metamac.statistical.operations.web.client.view;
 
 import org.siemac.metamac.statistical.operations.web.client.presenter.ErrorPagePresenter;
 import org.siemac.metamac.statistical.operations.web.client.view.handlers.ErrorPageUiHandlers;
+import org.siemac.metamac.web.common.client.MetamacWebCommon;
 
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
-import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.widgets.HTMLFlow;
+import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 public class ErrorPageViewImpl extends ViewWithUiHandlers<ErrorPageUiHandlers> implements ErrorPagePresenter.ErrorPageView {
@@ -15,7 +18,23 @@ public class ErrorPageViewImpl extends ViewWithUiHandlers<ErrorPageUiHandlers> i
     public ErrorPageViewImpl() {
         super();
         panel = new VLayout();
-        panel.addMember(new Label("Error"));
+
+        HLayout hLayout = new HLayout();
+        hLayout.setAlign(Alignment.CENTER);
+        hLayout.setMargin(20);
+
+        HTMLFlow htmlFlow = new HTMLFlow();
+        htmlFlow.setWidth(450);
+        htmlFlow.setMargin(40);
+        htmlFlow.setStyleName("exampleTextBlock");
+        StringBuffer contents = new StringBuffer();
+        contents.append("<hr>").append(MetamacWebCommon.getMessages().errorPageMessage()).append("<hr>");
+        htmlFlow.setContents(contents.toString());
+        htmlFlow.draw();
+
+        hLayout.addMember(htmlFlow);
+
+        panel.addMember(hLayout);
     }
 
     @Override
