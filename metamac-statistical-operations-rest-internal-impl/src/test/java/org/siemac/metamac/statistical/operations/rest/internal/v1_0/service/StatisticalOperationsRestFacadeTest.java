2c07437f90834e70f729aa0f63564898f8ea62ea
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-18 11:10:23 +0000

    In progress:     METAMAC-647 Esqueleto inicial del rest
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@37831 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeTest.java
+++ metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeTest.java
@@ -1,7 +1,6 @@
 package org.siemac.metamac.statistical.operations.rest.internal.v1_0.service;
 
-import javax.ws.rs.core.Response;
-
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
 import org.junit.BeforeClass;
@@ -23,54 +22,21 @@ public class StatisticalOperationsRestFacadeTest extends AbstractBusClientServer
     @Test
     public void testRetrieveOperationByCode() throws Exception {
 
-        // http://localhost:9001/api/internal/v1.0/operations/[CODE]
-        String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/operations/CODE-1/";
-        WebClient webClient = WebClient.create(baseAddress).accept("application/json");
-        Response response = webClient.get();
-        assertNotNull(response.getEntity().toString());
-        // Long id = wc.type("application/xml").accept("text/plain").post(new Book("CXF", 1L), Long.class);
-        // assertEquals(new Long(1), id);
-        // Book book = wc.accept("application/xml").query("id", 1L).get(Book.class);
-        // assertEquals("CXF", book.getName());
-
-        // Path("datastructure/{agencyID}/{resourceID}/{version}")
-
-        // String endpointAddress = "http://localhost:" + PORT + "/webapp/rest/datastructure/INE/KF_DIV_6/1.0";
-        //
-        // URL url = new URL(endpointAddress);
-        // URLConnection connect = url.openConnection();
-        // connect.addRequestProperty("Accept", "application/vnd.sdmx.structure+xml;version=2.1");
-        // InputStream in = connect.getInputStream();
-        // assertNotNull(in);
-        // System.out.println(getStringFromInputStream(in));
-
-        // InputStream expected = getClass().getResourceAsStream("resources/expected_get_book123.txt");
-
-        // assertEquals(getStringFromInputStream(expected), getStringFromInputStream(in));
+        String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/";
+        StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacade = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
+        WebClient.client(statisticalOperationsRestFacade).accept("application/xml");
+        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation operation = statisticalOperationsRestFacade.retrieveOperationByCode("CODE-1");
+        assertNotNull(operation);
 
     }
-
-    // private String getStringFromInputStream(InputStream in) throws Exception {
-    // CachedOutputStream bos = new CachedOutputStream();
-    // IOUtils.copy(in, bos);
-    // in.close();
-    // bos.close();
-    // //System.out.println(bos.getOut().toString());
-    // return bos.getOut().toString();
-    // }
-
+    
     @Test
     public void testFindOperations() throws Exception {
 
-        // http://localhost:9001/api/internal/v1.0/operations
-        String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/operations";
-        WebClient webClient = WebClient.create(baseAddress).accept("application/json");
-        Response response = webClient.get();
-        assertNotNull(response.getEntity().toString());
-        
-        webClient = WebClient.create(baseAddress).accept("application/xml");
-        response = webClient.get();
-        assertNotNull(response.getEntity().toString());
-
+        String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/";
+        StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacade = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
+        WebClient.client(statisticalOperationsRestFacade).accept("application/xml");
+        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operations operations = statisticalOperationsRestFacade.findOperations();
+        assertNotNull(operations);
     }
 }
