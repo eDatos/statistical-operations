3f085410a336601c4ab3398a47cf0fc812cac939
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-18 11:18:17 +0000

    In progress:     METAMAC-647 Esqueleto inicial del rest
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@37834 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeTest.java
+++ metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeTest.java
@@ -10,32 +10,32 @@ import org.siemac.metamac.common.test.rest.ServerResource;
 public class StatisticalOperationsRestFacadeTest extends AbstractBusClientServerTestBase {
 
     private static final String PORT = ServerResource.PORT;
+    private static String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/";
+    private static StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacade; 
 
     @BeforeClass
-    public static void startServers() throws Exception {
-        // JNDI SDMXDS
-        // SimpleNamingContextBuilder simpleNamingContextBuilder = OracleJNDIMock.setUp("SDMXDS", "metamac", "metamac", "jdbc:oracle:thin:@localhost:1521:XE", null);
+    public static void setUpBeforeClass() throws Exception {
 
+        // Start server
         assertTrue("server did not launch correctly", launchServer(ServerResource.class, true));
+        
+        // Rest client
+        statisticalOperationsRestFacade = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
+        WebClient.client(statisticalOperationsRestFacade).accept("application/xml");
     }
 
     @Test
     public void testRetrieveOperationByCode() throws Exception {
-
-        String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/";
-        StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacade = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
-        WebClient.client(statisticalOperationsRestFacade).accept("application/xml");
-        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation operation = statisticalOperationsRestFacade.retrieveOperationByCode("CODE-1");
+        String code = "NOT_EXISTS";
+        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation operation = statisticalOperationsRestFacade.retrieveOperationByCode(code);
+        
         assertNotNull(operation);
+        assertEquals(code, operation.getCode());
 
     }
     
     @Test
     public void testFindOperations() throws Exception {
-
-        String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/";
-        StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacade = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
-        WebClient.client(statisticalOperationsRestFacade).accept("application/xml");
         org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operations operations = statisticalOperationsRestFacade.findOperations();
         assertNotNull(operations);
     }
