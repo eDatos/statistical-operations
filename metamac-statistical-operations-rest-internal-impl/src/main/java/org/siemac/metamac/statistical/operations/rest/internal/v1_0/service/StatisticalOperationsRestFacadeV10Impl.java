ce507ef51a762291d12832540799fe4659080091
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-19 07:48:09 +0000

    In progress:     METAMAC-647 Esqueleto inicial del rest. Utilizando Mock!
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@37880 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeV10Impl.java
+++ metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeV10Impl.java
@@ -1,19 +1,12 @@
 package org.siemac.metamac.statistical.operations.rest.internal.v1_0.service;
 
 import java.math.BigInteger;
-import java.util.List;
 
-import org.fornax.cartridges.sculptor.framework.accessapi.ConditionalCriteria;
-import org.fornax.cartridges.sculptor.framework.accessapi.ConditionalCriteriaBuilder;
-import org.fornax.cartridges.sculptor.framework.accessapi.ConditionalCriteriaBuilder.ConditionRoot;
-import org.fornax.cartridges.sculptor.framework.domain.PagedResult;
-import org.fornax.cartridges.sculptor.framework.domain.PagingParameter;
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.domain.statistical.operations.enume.domain.ProcStatusEnum;
-import org.siemac.metamac.statistical.operations.core.domain.Operation;
-import org.siemac.metamac.statistical.operations.core.domain.OperationProperties;
 import org.siemac.metamac.statistical.operations.core.serviceapi.StatisticalOperationsBaseService;
+import org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation;
 import org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operations;
 import org.siemac.metamac.statistical.operations.rest.internal.v1_0.mapper.Do2RestInternalMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,38 +41,20 @@ public class StatisticalOperationsRestFacadeV10Impl implements StatisticalOperat
     }
 
     @Override
-    public org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation retrieveOperationByCode(String code) {
+    public Operation retrieveOperationByCode(String code) {
         try {
             // TODO Validation of parameters
 
-            // Conditions (by code and publish internally or externally)
-            ConditionRoot<Operation> criteria = ConditionalCriteriaBuilder.criteriaFor(Operation.class);
-            criteria.withProperty(OperationProperties.code()).eq(code);
-            criteria.and();
-            criteria.lbrace();
-            criteria.withProperty(OperationProperties.procStatus()).eq(ProcStatusEnum.PUBLISH_INTERNALLY);
-            criteria.or();
-            criteria.withProperty(OperationProperties.procStatus()).eq(ProcStatusEnum.PUBLISH_EXTERNALLY);
-            criteria.rbrace();
-            criteria.distinctRoot();
-            List<ConditionalCriteria> conditions = criteria.build();
-            // Only one result
-            PagingParameter pagingParameter = PagingParameter.pageAccess(1, 1);
-
-            // Find operations
-            PagedResult<Operation> result = statisticalOperationsBaseService.findOperationByCondition(serviceContextRestInternal, conditions, pagingParameter);
-            if (result != null && result.getValues() != null && result.getValues().size() == 1) {
-                Operation operation = result.getValues().get(0);
-                org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation operationBase = do2RestInternalMapper.operationToOperationBase(operation);
-                return operationBase;
-                
-            } else {
+            org.siemac.metamac.statistical.operations.core.domain.Operation operationEntity = statisticalOperationsBaseService.findOperationByCode(serviceContextRestInternal, code);
+            if (operationEntity == null || (!ProcStatusEnum.PUBLISH_EXTERNALLY.equals(operationEntity.getProcStatus()) && !ProcStatusEnum.PUBLISH_INTERNALLY.equals(operationEntity.getProcStatus()))) {
                 // TODO
-                org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation operationBase = new org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation();
-                operationBase.setCode("NOT_EXISTS");
-                return operationBase;
+                Operation operation = new org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation();
+                operation.setCode("NOT_EXISTS");
+                return operation;
 //                throw new MetamacException(ServiceExceptionType.OPERATION_NOT_FOUND, code);
             }
+            Operation operation = do2RestInternalMapper.operationToOperationBase(operationEntity);
+            return operation;
         } catch (MetamacException e) {
             // TODO error, con código y título
             // return Response.status(errorStatus).entity(sdmx21ErrorResponse).build();
--- metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeV10Test.java
+++ metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeV10Test.java
@@ -1,42 +1,67 @@
 package org.siemac.metamac.statistical.operations.rest.internal.v1_0.service;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
+import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.siemac.metamac.common.test.rest.ServerResource;
+import org.siemac.metamac.core.common.util.ApplicationContextProvider;
+import org.siemac.metamac.statistical.operations.core.domain.Operation;
+import org.siemac.metamac.statistical.operations.core.serviceapi.StatisticalOperationsBaseService;
+import org.springframework.context.ApplicationContext;
 
 public class StatisticalOperationsRestFacadeV10Test extends AbstractBusClientServerTestBase {
 
-    private static final String PORT = ServerResource.PORT;
-    private static String baseAddress = "http://localhost:" + PORT + "/api/internal/v1.0/";
-    private static StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacade; 
+    private static final String                       PORT               = ServerResource.PORT;
+    private static String                             baseAddress        = "http://localhost:" + PORT + "/api/internal/v1.0/";
+
+    private static ApplicationContext                 applicationContext = null;
+
+    private static StatisticalOperationsRestFacadeV10 statisticalOperationsRestFacadeClient;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
 
         // Start server
         assertTrue("server did not launch correctly", launchServer(ServerResource.class, true));
-        
+
+        // Get application context from Jetty
+        applicationContext = ApplicationContextProvider.getApplicationContext();
+
         // Rest client
-        statisticalOperationsRestFacade = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
-        WebClient.client(statisticalOperationsRestFacade).accept("application/xml");
+        statisticalOperationsRestFacadeClient = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
+        WebClient.client(statisticalOperationsRestFacadeClient).accept("application/xml");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+        // MOCKS
+        StatisticalOperationsBaseService statisticalOperationsBaseService = applicationContext.getBean(StatisticalOperationsBaseService.class);
+
+        when(statisticalOperationsBaseService.findOperationByCode(any(ServiceContext.class), eq("Code1"))).thenReturn(new Operation());
     }
 
     @Test
     public void testRetrieveOperationByCode() throws Exception {
+
         String code = "NOT_EXISTS";
-        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation operation = statisticalOperationsRestFacade.retrieveOperationByCode(code);
-        
+        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operation operation = statisticalOperationsRestFacadeClient.retrieveOperationByCode(code);
+
         assertNotNull(operation);
         assertEquals(code, operation.getCode());
-
     }
-    
+
     @Test
     public void testFindOperations() throws Exception {
-        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operations operations = statisticalOperationsRestFacade.findOperations();
+        org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operations operations = statisticalOperationsRestFacadeClient.findOperations();
         assertNotNull(operations);
     }
 }
