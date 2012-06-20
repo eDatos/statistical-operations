0721064ac7b76ccdd4278def4b29198b6edd49e3
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-20 08:52:34 +0000

    In progress:  METAMAC-676 API: obtener operación. Testear el string xml y json
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@37975 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeV10Test.java
+++ metamac-statistical-operations-rest-internal-impl/src/test/java/org/siemac/metamac/statistical/operations/rest/internal/v1_0/service/StatisticalOperationsRestFacadeV10Test.java
@@ -4,6 +4,13 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 
+import java.io.InputStream;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.io.CachedOutputStream;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.cxf.testutil.common.AbstractBusClientServerTestBase;
@@ -22,7 +29,10 @@ import org.springframework.context.ApplicationContext;
 public class StatisticalOperationsRestFacadeV10Test extends AbstractBusClientServerTestBase {
 
     private static final String                       PORT               = ServerResource.PORT;
-    private static String                             baseAddress        = "http://localhost:" + PORT + "/api/internal/v1.0/";
+    private static String                             baseApi            = "http://localhost:" + PORT + "/api/internal/v1.0";
+
+    private static String                             APPLICATION_XML    = "application/xml";
+    private static String                             APPLICATION_JSON   = "application/json";
 
     private static ApplicationContext                 applicationContext = null;
 
@@ -38,8 +48,8 @@ public class StatisticalOperationsRestFacadeV10Test extends AbstractBusClientSer
         applicationContext = ApplicationContextProvider.getApplicationContext();
 
         // Rest client
-        statisticalOperationsRestFacadeClient = JAXRSClientFactory.create(baseAddress, StatisticalOperationsRestFacadeV10.class);
-        WebClient.client(statisticalOperationsRestFacadeClient).accept("application/xml");
+        statisticalOperationsRestFacadeClient = JAXRSClientFactory.create(baseApi, StatisticalOperationsRestFacadeV10.class);
+        WebClient.client(statisticalOperationsRestFacadeClient).accept(APPLICATION_XML);
     }
 
     @Before
@@ -62,8 +72,57 @@ public class StatisticalOperationsRestFacadeV10Test extends AbstractBusClientSer
     }
 
     @Test
+    public void testRetrieveOperationByCodeReviewXml() throws Exception {
+
+        String code = "Code1";
+        String baseRequest = baseApi + "/operations/" + code;
+
+        WebClient webClient = WebClient.create(baseRequest).accept(APPLICATION_XML);
+        Response response = webClient.get();
+
+        // Validation
+        assertEquals(200, response.getStatus());
+        InputStream responseStream = (InputStream) response.getEntity();
+        String actual = getStringFromInputStream(responseStream);
+        String expected = getStringFromInputStream(StatisticalOperationsRestFacadeV10Test.class.getResourceAsStream("/responses/retrieveOperationByCode.code1.xml"));
+        // Only compare lenghts of response, because items in response can be received in different order (example: LocalisedString)
+        String actualFormatted = StringUtils.deleteWhitespace(actual);
+        String expectedFormatted = StringUtils.deleteWhitespace(expected);
+        assertEquals(actualFormatted.length(), expectedFormatted.length());
+    }
+
+    @Test
+    public void testRetrieveOperationByCodeReviewJson() throws Exception {
+
+        String code = "Code1";
+        String baseRequest = baseApi + "/operations/" + code;
+
+        WebClient webClient = WebClient.create(baseRequest).accept(APPLICATION_JSON);
+        Response response = webClient.get();
+
+        // Validation
+        assertEquals(200, response.getStatus());
+        InputStream responseStream = (InputStream) response.getEntity();
+        String actual = getStringFromInputStream(responseStream);
+        String expected = getStringFromInputStream(StatisticalOperationsRestFacadeV10Test.class.getResourceAsStream("/responses/retrieveOperationByCode.code1.json"));
+        // Only compare lenghts of response, because items in response can be received in different order (example: LocalisedString)
+        String actualFormatted = StringUtils.deleteWhitespace(actual);
+        String expectedFormatted = StringUtils.deleteWhitespace(expected);
+        assertEquals(actualFormatted.length(), expectedFormatted.length());
+    }
+
+    @Test
     public void testFindOperations() throws Exception {
         org.siemac.metamac.statistical.operations.rest.internal.v1_0.domain.Operations operations = statisticalOperationsRestFacadeClient.findOperations();
         assertNotNull(operations);
+    }
+
+    private String getStringFromInputStream(InputStream in) throws Exception {
+        CachedOutputStream bos = new CachedOutputStream();
+        IOUtils.copy(in, bos);
+        in.close();
+        bos.close();
+        // System.out.println(bos.getOut().toString());
+        return bos.getOut().toString();
     }
 }
