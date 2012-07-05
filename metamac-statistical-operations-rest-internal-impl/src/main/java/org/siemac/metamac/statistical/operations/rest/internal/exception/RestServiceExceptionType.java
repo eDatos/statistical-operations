b481a5ed2c8a76ed2762312ea895350acab62fc7
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-05 13:40:04 +0000

    METAMAC-768: Traducir los mensajes de error de la API
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@38484 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical/operations/rest/internal/exception/RestServiceExceptionType.java
+++ metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical/operations/rest/internal/exception/RestServiceExceptionType.java
@@ -4,7 +4,7 @@ import org.siemac.metamac.rest.exception.RestCommonServiceExceptionType;
 
 public class RestServiceExceptionType extends RestCommonServiceExceptionType {
 
-    public static final RestCommonServiceExceptionType FAMILY_NOT_FOUND    = create("exception.rest_operations.family.not_found");
-    public static final RestCommonServiceExceptionType OPERATION_NOT_FOUND = create("exception.rest_operations.operation.not_found");
-    public static final RestCommonServiceExceptionType INSTANCE_NOT_FOUND  = create("exception.rest_operations.instance.not_found");
+    public static final RestCommonServiceExceptionType FAMILY_NOT_FOUND    = create("exception.operations.family.not_found");
+    public static final RestCommonServiceExceptionType OPERATION_NOT_FOUND = create("exception.operations.operation.not_found");
+    public static final RestCommonServiceExceptionType INSTANCE_NOT_FOUND  = create("exception.operations.instance.not_found");
 }
