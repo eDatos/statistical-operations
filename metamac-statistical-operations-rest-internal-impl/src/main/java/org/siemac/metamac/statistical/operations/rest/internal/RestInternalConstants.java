69e93aed8adc0c59388cdd70db420d2b01b225e7
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-18 13:43:21 +0000

    Refactor RestConstants a paquete de constantes. Añadido "kind" a constantes
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@38909 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical/operations/rest/internal/RestInternalConstants.java
+++ metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical/operations/rest/internal/RestInternalConstants.java
@@ -1,5 +1,7 @@
 package org.siemac.metamac.statistical.operations.rest.internal;
 
+import org.siemac.metamac.rest.constants.RestConstants;
+
 public class RestInternalConstants {
 
     public static String  LINK_SUBPATH_OPERATIONS = "operations";
@@ -8,17 +10,16 @@ public class RestInternalConstants {
 
     private static String API_NAME                = "statisticalOperations";
 
-    private static String KIND_SEPARATOR          = "#";
-    public static String  KIND_OPERATIONS         = API_NAME + KIND_SEPARATOR + "operations";
-    public static String  KIND_OPERATION          = API_NAME + KIND_SEPARATOR + "operation";
-    public static String  KIND_FAMILIES           = API_NAME + KIND_SEPARATOR + "families";
-    public static String  KIND_FAMILY             = API_NAME + KIND_SEPARATOR + "family";
-    public static String  KIND_INSTANCE           = API_NAME + KIND_SEPARATOR + "instance";
-    public static String  KIND_INSTANCES          = API_NAME + KIND_SEPARATOR + "instances";
-    public static String  KIND_SURVEY_TYPES       = API_NAME + KIND_SEPARATOR + "surveyTypes";
-    public static String  KIND_OFFICIALITY_TYPES  = API_NAME + KIND_SEPARATOR + "officialityTypes";
-    public static String  KIND_INSTANCE_TYPES     = API_NAME + KIND_SEPARATOR + "instanceTypes";
-    public static String  KIND_SURVEY_SOURCES     = API_NAME + KIND_SEPARATOR + "surveySources";
-    public static String  KIND_COLL_METHODS       = API_NAME + KIND_SEPARATOR + "collMethods";
-    public static String  KIND_COSTS              = API_NAME + KIND_SEPARATOR + "costs";
+    public static String  KIND_OPERATIONS         = API_NAME + RestConstants.KIND_SEPARATOR + "operations";
+    public static String  KIND_OPERATION          = API_NAME + RestConstants.KIND_SEPARATOR + "operation";
+    public static String  KIND_FAMILIES           = API_NAME + RestConstants.KIND_SEPARATOR + "families";
+    public static String  KIND_FAMILY             = API_NAME + RestConstants.KIND_SEPARATOR + "family";
+    public static String  KIND_INSTANCE           = API_NAME + RestConstants.KIND_SEPARATOR + "instance";
+    public static String  KIND_INSTANCES          = API_NAME + RestConstants.KIND_SEPARATOR + "instances";
+    public static String  KIND_SURVEY_TYPES       = API_NAME + RestConstants.KIND_SEPARATOR + "surveyTypes";
+    public static String  KIND_OFFICIALITY_TYPES  = API_NAME + RestConstants.KIND_SEPARATOR + "officialityTypes";
+    public static String  KIND_INSTANCE_TYPES     = API_NAME + RestConstants.KIND_SEPARATOR + "instanceTypes";
+    public static String  KIND_SURVEY_SOURCES     = API_NAME + RestConstants.KIND_SEPARATOR + "surveySources";
+    public static String  KIND_COLL_METHODS       = API_NAME + RestConstants.KIND_SEPARATOR + "collMethods";
+    public static String  KIND_COSTS              = API_NAME + RestConstants.KIND_SEPARATOR + "costs";
 }
