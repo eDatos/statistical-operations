b1fa9a67e6bcaac5a536dc1cd51e8f22279b35f7
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-13 12:58:53 +0000

    Refactor en scripts SQL
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@41772 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ etc/db/oracle/01-create/03b-create-sequence-function.sql
@@ -0,0 +1,11 @@
+-- Create GET_NEXT_SEQUENCE_VALUE function
+
+CREATE OR REPLACE FUNCTION GET_NEXT_SEQUENCE_VALUE(sequence_name_in IN VARCHAR2)
+  RETURN NUMBER
+  IS sequence_next_value_out NUMBER(19,0);
+  BEGIN
+    SELECT SEQUENCE_NEXT_VALUE INTO sequence_next_value_out
+    FROM TB_SEQUENCES 
+    WHERE SEQUENCE_NAME = sequence_name_in;
+    RETURN (sequence_next_value_out);
+  END;
