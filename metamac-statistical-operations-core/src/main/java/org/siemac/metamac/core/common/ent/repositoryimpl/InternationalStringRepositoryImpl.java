caf401a8ab0e8d6894d4b734989b4400cf4dc6b6
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-04-22 08:30:00 +0000

    METAMAC-1508: [CODES] Normalización de clasificaciones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@45296 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-statistical-operations-core/src/main/java/org/siemac/metamac/core/common/ent/repositoryimpl/InternationalStringRepositoryImpl.java
@@ -0,0 +1,20 @@
+package org.siemac.metamac.core.common.ent.repositoryimpl;
+
+import java.util.Collection;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository implementation for InternationalString
+ */
+@Repository("internationalStringRepository")
+public class InternationalStringRepositoryImpl extends InternationalStringRepositoryBase {
+
+    public InternationalStringRepositoryImpl() {
+    }
+
+    @Override
+    public void deleteInternationalStringsEfficiently(Collection<Long> internationalStringToDelete) {
+        // not necessary
+    }
+}
