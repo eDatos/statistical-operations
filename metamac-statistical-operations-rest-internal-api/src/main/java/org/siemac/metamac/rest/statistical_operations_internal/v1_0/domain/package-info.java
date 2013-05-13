a5847592c55ec523dde73a0cc44c7d02454e7a24
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-05-13 13:56:39 +0000

    METAMAC-916: Integración con API SRM. Separación de proyecto api por dependencia cíclica
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@45855 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-internal-api/src/main/java/org/siemac/metamac/rest/statistical_operations_internal/v1_0/domain/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siemac.org/metamac/rest/statistical-operations-internal/v1.0/domain", xmlns = {
-        @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.siemac.org/metamac/rest/statistical-operations-internal/v1.0/domain", prefix = "operations")})
-package org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain;
