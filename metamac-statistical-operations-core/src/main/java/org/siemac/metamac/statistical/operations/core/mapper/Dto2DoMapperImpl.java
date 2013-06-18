a4ce183d89c2c6bd5d6bf97f699c28610f4e7220
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-18 14:13:44 +0000

    METAMAC-1747: InternationalString: Limpiar tags html que no se soportan en Metamac
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@46920 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-core/src/main/java/org/siemac/metamac/statistical/operations/core/mapper/Dto2DoMapperImpl.java
+++ metamac-statistical-operations-core/src/main/java/org/siemac/metamac/statistical/operations/core/mapper/Dto2DoMapperImpl.java
@@ -655,6 +655,9 @@ public class Dto2DoMapperImpl extends BaseDto2DoMapperImpl implements Dto2DoMapp
     // ------------------------------------------------------------
 
     private InternationalString internationalStringToEntity(InternationalStringDto source, InternationalString target, String metadataName) throws MetamacException {
+        // Preprocess international string
+        internationalStringHtmlToTextPlain(source);
+
         // Check it is valid
         checkInternationalStringDtoValid(source, metadataName);
 
