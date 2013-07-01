6e8ac55bc9ceba4777419350b8bfedba9d0cbc3d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-01 11:01:16 +0000

    Format
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@47162 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/gin/OperationsWebGinjector.java
+++ metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/gin/OperationsWebGinjector.java
@@ -13,13 +13,10 @@ import org.siemac.metamac.statistical.operations.web.client.presenter.MainPagePr
 import org.siemac.metamac.statistical.operations.web.client.presenter.UnauthorizedPagePresenter;
 import org.siemac.metamac.web.common.client.gin.MetamacWebGinjector;
 
-import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.inject.client.AsyncProvider;
 import com.google.gwt.inject.client.GinModules;
 import com.google.inject.Provider;
 import com.gwtplatform.dispatch.client.gin.DispatchAsyncModule;
-import com.gwtplatform.dispatch.shared.DispatchAsync;
-import com.gwtplatform.mvp.client.proxy.PlaceManager;
 
 @GinModules({DispatchAsyncModule.class, ClientModule.class})
 public interface OperationsWebGinjector extends MetamacWebGinjector {
@@ -40,5 +37,4 @@ public interface OperationsWebGinjector extends MetamacWebGinjector {
     // Interfaces
     public OperationsWebConstants getOperationsWebConstants();
     public OperationsWebMessages getOperationsWebMessages();
-
 }
