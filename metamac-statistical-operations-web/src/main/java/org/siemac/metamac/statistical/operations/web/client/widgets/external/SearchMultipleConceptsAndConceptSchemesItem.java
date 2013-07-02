74c879ae846257a8ed46560b5bc8f0d40b667912
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-02 07:49:48 +0000

    METAMAC-1757 Corregido error en popup de selección de conceptos y esquemas de conceptos (para el metadato definiciones de conceptos estadísticos)
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@47168 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/widgets/external/SearchMultipleConceptsAndConceptSchemesItem.java
+++ metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/widgets/external/SearchMultipleConceptsAndConceptSchemesItem.java
@@ -2,33 +2,44 @@ package org.siemac.metamac.statistical.operations.web.client.widgets.external;
 
 import static org.siemac.metamac.statistical.operations.web.client.OperationsWeb.getConstants;
 
+import java.util.LinkedHashMap;
+
 import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
 import org.siemac.metamac.core.common.util.shared.StringUtils;
+import org.siemac.metamac.web.common.client.widgets.form.fields.CustomSelectItem;
 import org.siemac.metamac.web.common.client.widgets.form.fields.external.MultipleExternalResourceAction;
 import org.siemac.metamac.web.common.client.widgets.form.fields.external.SearchMultipleSrmItemsItem;
 import org.siemac.metamac.web.common.shared.criteria.SrmItemRestCriteria;
 import org.siemac.metamac.web.common.shared.criteria.SrmItemSchemeRestCriteria;
 import org.siemac.metamac.web.common.shared.domain.ExternalItemsResult;
 
+import com.smartgwt.client.widgets.form.fields.FormItem;
 import com.smartgwt.client.widgets.form.fields.events.ChangedEvent;
 import com.smartgwt.client.widgets.form.fields.events.ChangedHandler;
 
 public class SearchMultipleConceptsAndConceptSchemesItem extends SearchMultipleSrmItemsItem {
 
-    private static final TypeExternalArtefactsEnum DEFAULT_INITIAL_RESOURCE_TYPE = TypeExternalArtefactsEnum.CONCEPT;
+    private static final String       RESOURCE_TYPE_FIELD_NAME = "resource-type";
 
-    private SrmItemSchemeRestCriteria              itemSchemeCriteria;
-    private SrmItemRestCriteria                    itemCriteria;
+    private SrmItemSchemeRestCriteria itemSchemeCriteria;
+    private SrmItemRestCriteria       itemCriteria;
 
     public SearchMultipleConceptsAndConceptSchemesItem(final String name, String title, SrmItemSchemeRestCriteria itemSchemeCriteria, SrmItemRestCriteria itemCriteria,
             MultipleExternalResourceAction action) {
         super(name, title, itemSchemeCriteria, itemCriteria, getConstants().searchConceptsAndConceptSchemes(), getConstants().filterConceptScheme(), getConstants().selectedConceptScheme(),
-                getConstants().selectionResources(), new ConceptAndConceptSchemeSelectItem(DEFAULT_INITIAL_RESOURCE_TYPE), action);
+                getConstants().selectionResources(), action);
 
         this.itemSchemeCriteria = itemSchemeCriteria;
         this.itemCriteria = itemCriteria;
+    }
 
-        getInitialFilterSelectItem().addChangedHandler(new ChangedHandler() {
+    @Override
+    public FormItem[] getFormItemtsToAddToInitialFilterForm() {
+        CustomSelectItem resourceType = new CustomSelectItem(RESOURCE_TYPE_FIELD_NAME, getConstants().resourceTypeToAdd());
+        resourceType.setValueMap(getConceptAndConceptSchemeValueMap());
+        resourceType.setValue(TypeExternalArtefactsEnum.CONCEPT.name());
+        resourceType.setVisible(true);
+        resourceType.addChangedHandler(new ChangedHandler() {
 
             @Override
             public void onChanged(ChangedEvent event) {
@@ -45,18 +56,20 @@ public class SearchMultipleConceptsAndConceptSchemesItem extends SearchMultipleS
 
                     if (TypeExternalArtefactsEnum.CONCEPT_SCHEME.equals(selectedResourceType)) {
                         searchMultipleItemsWindow.getFilterForm().hide();
-                        getUiHandlers().retrieveItemSchemes(name, SearchMultipleConceptsAndConceptSchemesItem.this.itemSchemeCriteria, FIRST_RESULT, MAX_RESULTS);
+                        getUiHandlers().retrieveItemSchemes(getName(), SearchMultipleConceptsAndConceptSchemesItem.this.itemSchemeCriteria, FIRST_RESULT, MAX_RESULTS);
                     }
                     if (TypeExternalArtefactsEnum.CONCEPT.equals(selectedResourceType)) {
                         searchMultipleItemsWindow.getFilterForm().show();
-                        getUiHandlers().retrieveItemSchemes(name, SearchMultipleConceptsAndConceptSchemesItem.this.itemSchemeCriteria, FIRST_RESULT, MAX_RESULTS);
-                        getUiHandlers().retrieveItems(name, SearchMultipleConceptsAndConceptSchemesItem.this.itemCriteria, FIRST_RESULT, MAX_RESULTS);
+                        getUiHandlers().retrieveItemSchemes(getName(), SearchMultipleConceptsAndConceptSchemesItem.this.itemSchemeCriteria, FIRST_RESULT, MAX_RESULTS);
+                        getUiHandlers().retrieveItems(getName(), SearchMultipleConceptsAndConceptSchemesItem.this.itemCriteria, FIRST_RESULT, MAX_RESULTS);
                     }
 
                     searchMultipleItemsWindow.markForRedraw();
                 }
             }
         });
+        FormItem[] formItems = new FormItem[]{resourceType};
+        return formItems;
     }
 
     /**
@@ -85,14 +98,23 @@ public class SearchMultipleConceptsAndConceptSchemesItem extends SearchMultipleS
     }
 
     private TypeExternalArtefactsEnum getSelectedTypeExternalArtefactsEnum() {
-        String value = getInitialFilterSelectItem().getValueAsString();
-        if (!StringUtils.isBlank(value)) {
-            try {
-                return TypeExternalArtefactsEnum.valueOf(value);
-            } catch (Exception e) {
-                return null;
+        if (searchMultipleItemsWindow.getInitialFilterForm().getItem(RESOURCE_TYPE_FIELD_NAME).getValue() != null) {
+            String value = (String) searchMultipleItemsWindow.getInitialFilterForm().getItem(RESOURCE_TYPE_FIELD_NAME).getValue();
+            if (!StringUtils.isBlank(value)) {
+                try {
+                    return TypeExternalArtefactsEnum.valueOf(value);
+                } catch (Exception e) {
+                    return null;
+                }
             }
         }
         return null;
+    }
+
+    private LinkedHashMap<String, String> getConceptAndConceptSchemeValueMap() {
+        LinkedHashMap<String, String> valueMap = new LinkedHashMap<String, String>();
+        valueMap.put(TypeExternalArtefactsEnum.CONCEPT_SCHEME.name(), getConstants().conceptSchemes());
+        valueMap.put(TypeExternalArtefactsEnum.CONCEPT.name(), getConstants().concepts());
+        return valueMap;
     }
 }
