20b48642cece806425ead4e950f2fabc01550314
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-12 09:07:54 +0000

    Utilidad para transformar una excepción desde un cliente de api a RestException
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@47514 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-external-impl/src/main/java/org/siemac/metamac/statistical_operations/rest/external/invocation/CommonMetadataRestExternalFacadeImpl.java
+++ metamac-statistical-operations-rest-external-impl/src/main/java/org/siemac/metamac/statistical_operations/rest/external/invocation/CommonMetadataRestExternalFacadeImpl.java
@@ -1,13 +1,9 @@
 package org.siemac.metamac.statistical_operations.rest.external.invocation;
 
-import javax.ws.rs.core.Response.Status;
-
-import org.apache.cxf.jaxrs.client.ServerWebApplicationException;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.siemac.metamac.rest.common_metadata.v1_0.domain.Configuration;
 import org.siemac.metamac.rest.exception.RestException;
 import org.siemac.metamac.rest.exception.utils.RestExceptionUtils;
-import org.siemac.metamac.statistical_operations.rest.external.exception.RestServiceExceptionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +21,13 @@ public class CommonMetadataRestExternalFacadeImpl implements CommonMetadataRestE
     public Configuration retrieveConfigurationById(String id) {
         try {
             return restApiLocator.getCommonMetadataRestExternalFacadeV10().retrieveConfigurationById(id); // CONFIGURATION ID in the rest API is what we call CODE
-        } catch (ServerWebApplicationException e) {
-            logger.error("Error", e);
-            org.siemac.metamac.rest.common.v1_0.domain.Exception exception = e.toErrorObject(WebClient.client(restApiLocator.getCommonMetadataRestExternalFacadeV10()),
-                    org.siemac.metamac.rest.common.v1_0.domain.Exception.class);
-            if (exception == null) {
-                exception = RestExceptionUtils.getException(RestServiceExceptionType.UNKNOWN);
-            }
-            throw new RestException(exception, Status.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            throw toRestException(e);
         }
+    }
+
+    private RestException toRestException(Exception e) {
+        logger.error("Error", e);
+        return RestExceptionUtils.toRestException(e, WebClient.client(restApiLocator.getCommonMetadataRestExternalFacadeV10()));
     }
 }
--- metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_operations/rest/internal/invocation/SrmRestInternalFacadeImpl.java
+++ metamac-statistical-operations-rest-internal-impl/src/main/java/org/siemac/metamac/statistical_operations/rest/internal/invocation/SrmRestInternalFacadeImpl.java
@@ -3,16 +3,12 @@ package org.siemac.metamac.statistical_operations.rest.internal.invocation;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.core.Response.Status;
-
-import org.apache.cxf.jaxrs.client.ServerWebApplicationException;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.siemac.metamac.core.common.util.shared.UrnUtils;
 import org.siemac.metamac.rest.exception.RestException;
 import org.siemac.metamac.rest.exception.utils.RestExceptionUtils;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.Concepts;
 import org.siemac.metamac.rest.structural_resources_internal.v1_0.domain.ResourceInternal;
-import org.siemac.metamac.statistical_operations.rest.internal.exception.RestServiceExceptionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,14 +39,13 @@ public class SrmRestInternalFacadeImpl implements SrmRestInternalFacade {
                 offset += concepts.getConcepts().size(); // next page
             } while (concepts.getTotal().intValue() != results.size());
             return results;
-        } catch (ServerWebApplicationException e) {
-            logger.error("Error", e);
-            org.siemac.metamac.rest.common.v1_0.domain.Exception exception = e.toErrorObject(WebClient.client(restApiLocator.getSrmRestInternalFacadeV10()),
-                    org.siemac.metamac.rest.common.v1_0.domain.Exception.class);
-            if (exception == null) {
-                exception = RestExceptionUtils.getException(RestServiceExceptionType.UNKNOWN);
-            }
-            throw new RestException(exception, Status.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            throw toRestException(e);
         }
+    }
+
+    private RestException toRestException(Exception e) {
+        logger.error("Error", e);
+        return RestExceptionUtils.toRestException(e, WebClient.client(restApiLocator.getSrmRestInternalFacadeV10()));
     }
 }
