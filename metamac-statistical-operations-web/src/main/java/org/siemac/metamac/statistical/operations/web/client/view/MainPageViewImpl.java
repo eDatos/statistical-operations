290ebff5c2f7b6889648f07ef59a719c42f00e27
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-28 14:17:57 +0000

    METAMAC-1873 Aumentar tiempo de espera para ocultar mensaje de éxito
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@48973 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/view/MainPageViewImpl.java
+++ metamac-statistical-operations-web/src/main/java/org/siemac/metamac/statistical/operations/web/client/view/MainPageViewImpl.java
@@ -189,7 +189,7 @@ public class MainPageViewImpl extends ViewWithUiHandlers<MainPageUiHandlers> imp
                     successMessagePanel.animateHide(AnimationEffect.FADE);
                 }
             };
-            timer.schedule(6000);
+            timer.schedule(12000);
         } else if (MessageTypeEnum.ERROR.equals(type)) {
             errorMessagePanel.showMessage(throwable);
         }
