00b59f75fc793e4b6f42cee1ef4948db9fbe6b12
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-02-05 11:10:33 +0000

    METAMAC-2109 - Problemas actualización desde versión anterior instalada a nueva versión enviada
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@50980 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ etc/changes-from-release/1.4.0/db/statistical-operations/20140204-01.sql
@@ -0,0 +1,8 @@
+-- --------------------------------------------------------------------------------------------------
+-- METAMAC-2109 - Problemas actualización desde versión anterior instalada a nueva versión enviada
+-- --------------------------------------------------------------------------------------------------
+
+
+
+/* URN opcional */
+ALTER TABLE TB_EXTERNAL_ITEMS MODIFY URN VARCHAR2(4000) NULL;
