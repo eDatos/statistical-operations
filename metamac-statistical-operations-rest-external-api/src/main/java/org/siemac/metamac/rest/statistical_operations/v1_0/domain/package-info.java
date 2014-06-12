c0e727597b61bdf0c6c6c6a0c22ea8f09b99c65f
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-06-12 08:29:14 +0000

    Extracción del api externa
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@52394 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-statistical-operations-rest-external-api/src/main/java/org/siemac/metamac/rest/statistical_operations/v1_0/domain/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siemac.org/metamac/rest/statistical-operations/v1.0/domain", xmlns = {
-        @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.siemac.org/metamac/rest/statistical-operations/v1.0/domain", prefix = "operations")})
-package org.siemac.metamac.rest.statistical_operations.v1_0.domain;
