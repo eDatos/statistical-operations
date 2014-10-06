09de45ef8b89b135c60b8d7270c748ab4a607791
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-10-06 11:31:18 +0000

    METAMAC-2369 - [BBDD] Los scripts de MSSQL contienen errores >> Eliminar commit de scipts de MSSQL
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@53415 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/1.6.4/db/common-metadata/mssql/20140402.sql
+++ etc/changes-from-release/1.6.4/db/common-metadata/mssql/20140402.sql
@@ -4,5 +4,3 @@
 
 insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'),1,1,'metamac.data.docs.statistical-operations.path','${metamac.data.path}/statistical-operations/docs');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
-
-commit;
