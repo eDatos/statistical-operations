8ed74bb64c7efc499ba574e1ef0ac216fd42b678
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-02-15 08:56:40 +0000

    METAMAC-2347 - [OOEE] Las operaciones deben tener un código de como máximo 11 dígitos > Corregido el script de MSSQL
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-statistical-operations/trunk/metamac-statistical-operations@54752 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/1.6.4/db/statistical-operations/mssql/20140910.sql
+++ etc/changes-from-release/1.6.4/db/statistical-operations/mssql/20140910.sql
@@ -1,4 +1,16 @@
 -- --------------------------------------------------------------------------------------
 -- METAMAC-2347 - [OOEE] Las operaciones deben tener un código de como máximo 11 dígitos
 -- --------------------------------------------------------------------------------------
-ALTER TABLE TB_OPERATIONS ALTER COLUMN CODE NVARCHAR(11);
+
+-- Se debe comprobar primero si hay alguna reestricción que haga uso de la columna a modificar 
+
+IF EXISTS (
+  SELECT * FROM sys.objects WHERE NAME = 'OPERATIONS_CODE'
+)
+ALTER TABLE JENKINS_METAMAC_STATISTICAL_OPERATIONS.TB_OPERATIONS DROP CONSTRAINT OPERATIONS_CODE
+GO
+ALTER TABLE JENKINS_METAMAC_STATISTICAL_OPERATIONS.TB_OPERATIONS ALTER COLUMN CODE NVARCHAR(11)
+GO
+ALTER TABLE JENKINS_METAMAC_STATISTICAL_OPERATIONS.TB_OPERATIONS
+  ADD CONSTRAINT OPERATIONS_CODE
+  UNIQUE(CODE)
