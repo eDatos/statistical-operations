7069261feff63224f338861b82411f39e5839985
F Javier Rodriguez Garcia <frodgar@arte-consultores.com>
2016-08-04 09:51:10 +0100

    METAMAC-2485 - metamac-web-common no debe ser dependencia de proyectos que no sean GWT > Eliminada dependencia de metamac-statistical-operations

--- metamac-statistical-operations-external-web/src/main/java/org/siemac/metamac/statistical/operations/web/external/ApplicationStartup.java
+++ metamac-statistical-operations-external-web/src/main/java/org/siemac/metamac/statistical/operations/web/external/ApplicationStartup.java
@@ -4,47 +4,47 @@ import javax.servlet.ServletContextEvent;
 
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.core.common.listener.ApplicationStartupListener;
-import org.siemac.metamac.statistical.operations.core.constants.StatisticalOperationsConfigurationConstants;
 import org.siemac.metamac.core.common.util.WebUtils;
+import org.siemac.metamac.statistical.operations.core.constants.StatisticalOperationsConfigurationConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ApplicationStartup extends ApplicationStartupListener {
 
-	private static final Logger log = LoggerFactory.getLogger(ApplicationStartup.class);
+    private static final Logger log = LoggerFactory.getLogger(ApplicationStartup.class);
 
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
-		super.contextInitialized(sce);
-		try {
-			WebUtils.setOrganisation(configurationService.retrieveOrganisation());
-			WebUtils.setApiBaseURL(configurationService.retrieveStatisticalOperationsExternalApiUrlBase());
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        super.contextInitialized(sce);
+        try {
+            WebUtils.setOrganisation(configurationService.retrieveOrganisation());
+            WebUtils.setApiBaseURL(configurationService.retrieveStatisticalOperationsExternalApiUrlBase());
 
-			WebUtils.setApiStyleHeaderUrl(configurationService.retrieveApiStyleHeaderUrl());
-			WebUtils.setApiStyleCssUrl(configurationService.retrieveApiStyleCssUrl());
-			WebUtils.setApiStyleFooterUrl(configurationService.retrieveApiStyleFooterUrl());
-		} catch (MetamacException e) {
-			log.error("Error retrieving application configuration", e);
-		}
-	}
+            WebUtils.setApiStyleHeaderUrl(configurationService.retrieveApiStyleHeaderUrl());
+            WebUtils.setApiStyleCssUrl(configurationService.retrieveApiStyleCssUrl());
+            WebUtils.setApiStyleFooterUrl(configurationService.retrieveApiStyleFooterUrl());
+        } catch (MetamacException e) {
+            log.error("Error retrieving application configuration", e);
+        }
+    }
 
-	@Override
-	public String projectName() {
-		return "statistical-operations";
-	}
+    @Override
+    public String projectName() {
+        return "statistical-operations";
+    }
 
-	@Override
-	public void checkApplicationProperties() throws MetamacException {
-		// Datasource
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_DRIVER_NAME);
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_URL);
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_USERNAME);
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_PASSWORD);
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_DIALECT);
+    @Override
+    public void checkApplicationProperties() throws MetamacException {
+        // Datasource
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_DRIVER_NAME);
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_URL);
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_USERNAME);
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_PASSWORD);
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.DB_DIALECT);
 
-		// Api
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.ENDPOINT_STATISTICAL_OPERATIONS_EXTERNAL_API);
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.ENDPOINT_COMMON_METADATA_EXTERNAL_API);
-		checkRequiredProperty(StatisticalOperationsConfigurationConstants.ENDPOINT_SRM_EXTERNAL_API);
-	}
+        // Api
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.ENDPOINT_STATISTICAL_OPERATIONS_EXTERNAL_API);
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.ENDPOINT_COMMON_METADATA_EXTERNAL_API);
+        checkRequiredProperty(StatisticalOperationsConfigurationConstants.ENDPOINT_SRM_EXTERNAL_API);
+    }
 }
