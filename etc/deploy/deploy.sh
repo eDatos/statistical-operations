2c5aff4a28461e9511188d36f9832e56ce231c33
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-04-01 14:38:37 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Modificaciones en el fichero deploy.sh para adaptarlo al nuevo entorno de estadísticas

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -2,7 +2,8 @@
 
 HOME_PATH=metamac-statistical-operations
 TRANSFER_PATH=$HOME_PATH/tmp
-DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
+DEPLOY_TARGET_PATH_EXTERNAL=/servers/edatos-external/tomcats/edatos-external01/webapps
+DEPLOY_TARGET_PATH_INTERNAL=/servers/edatos-internal/tomcats/edatos-internal01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
 RESTART=1
@@ -19,48 +20,55 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
-
-    if [ $RESTART -eq 1 ]; then
-        sudo service metamac01 stop
-        checkPROC "metamac"
-    fi
-
+ 
     ###
     # STATISTICAL-OPERATIONS-INTERNAL
     ###
-
-    # Update Process
-    sudo rm -rf $DEPLOY_TARGET_PATH/statistical-operations-internal
-    sudo mv $TRANSFER_PATH/statistical-operations-internal.war $DEPLOY_TARGET_PATH/statistical-operations-internal.war
-    sudo unzip $DEPLOY_TARGET_PATH/statistical-operations-internal.war -d $DEPLOY_TARGET_PATH/statistical-operations-internal
-    sudo rm -rf $DEPLOY_TARGET_PATH/statistical-operations-internal.war
-
-    # Restore Configuration
-    sudo cp $HOME_PATH/environment_internal.xml $DEPLOY_TARGET_PATH/statistical-operations-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH/statistical-operations-internal/$LOGBACK_RELATIVE_PATH_FILE
-
-
-    ###
-    # STATISTICAL-OPERATIONS-EXTERNA
-    ###
-
-    # Update Process
-    sudo rm -rf $DEPLOY_TARGET_PATH/statistical-operations
-    sudo mv $TRANSFER_PATH/statistical-operations.war $DEPLOY_TARGET_PATH/statistical-operations.war
-    sudo unzip $DEPLOY_TARGET_PATH/statistical-operations.war -d $DEPLOY_TARGET_PATH/statistical-operations
-    sudo rm -rf $DEPLOY_TARGET_PATH/statistical-operations.war
-
-    # Restore Configuration
-    sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/statistical-operations/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/statistical-operations/$LOGBACK_RELATIVE_PATH_FILE
-
+    
     if [ $RESTART -eq 1 ]; then
-        sudo chown -R metamac.metamac /servers/metamac
-        sudo service metamac01 start
+        sudo service edatos-internal01 stop
+        checkPROC "edatos-internal"
     fi
 
-    #checkURL "http://estadisticas.arte-consultores.com/statistical-operations-internal" "metamac01"
-    #checkURL "http://estadisticas.arte-consultores.com/statistical-operations" "metamac01"
+    # Update Process
+    sudo rm -rf $DEPLOY_TARGET_PATH_INTERNAL/statistical-operations-internal
+    sudo mv $TRANSFER_PATH/statistical-operations-internal.war $DEPLOY_TARGET_PATH_INTERNAL/statistical-operations-internal.war
+    sudo unzip $DEPLOY_TARGET_PATH_INTERNAL/statistical-operations-internal.war -d $DEPLOY_TARGET_PATH_INTERNAL/statistical-operations-internal
+    sudo rm -rf $DEPLOY_TARGET_PATH_INTERNAL/statistical-operations-internal.war
+
+    # Restore Configuration
+    sudo cp $HOME_PATH/environment_internal.xml $DEPLOY_TARGET_PATH_INTERNAL/statistical-operations-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH_INTERNAL/statistical-operations-internal/$LOGBACK_RELATIVE_PATH_FILE
+
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R edatos-internal.edatos-internal /servers/edatos-internal     
+        sudo service edatos-internal01 start
+    fi
+
+    ###
+    # STATISTICAL-OPERATIONS-EXTERNAL
+    ###
+    
+    if [ $RESTART -eq 1 ]; then
+        sudo service edatos-external01 stop
+        checkPROC "edatos-external"
+    fi
+
+    # Update Process
+    sudo rm -rf $DEPLOY_TARGET_PATH_EXTERNAL/statistical-operations
+    sudo mv $TRANSFER_PATH/statistical-operations.war $DEPLOY_TARGET_PATH_EXTERNAL/statistical-operations.war
+    sudo unzip $DEPLOY_TARGET_PATH_EXTERNAL/statistical-operations.war -d $DEPLOY_TARGET_PATH_EXTERNAL/statistical-operations
+    sudo rm -rf $DEPLOY_TARGET_PATH_EXTERNAL/statistical-operations.war
+
+    # Restore Configuration
+    sudo cp $HOME_PATH/environment_external.xml $DEPLOY_TARGET_PATH_EXTERNAL/statistical-operations/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/logback_external.xml $DEPLOY_TARGET_PATH_EXTERNAL/statistical-operations/$LOGBACK_RELATIVE_PATH_FILE
+
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R edatos-external.edatos-external /servers/edatos-external        
+        sudo service edatos-external01 start
+    fi
+
     echo "Finished deploy"
 
 EOF
