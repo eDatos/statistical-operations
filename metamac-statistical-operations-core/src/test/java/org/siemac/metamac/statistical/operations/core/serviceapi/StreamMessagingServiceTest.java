9921df9535692c54d8d6e8b5d084011caccd409b
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-07-27 10:50:24 +0100

    EDATOS-3380 - Enviar mensaje a través de kafka al publicar operaciones
    > Añade un mensaje a los tests de Kafka advirtiendo de que no se testea.

--- metamac-statistical-operations-core/src/test/java/org/siemac/metamac/statistical/operations/core/serviceapi/StreamMessagingServiceTest.java
+++ metamac-statistical-operations-core/src/test/java/org/siemac/metamac/statistical/operations/core/serviceapi/StreamMessagingServiceTest.java
@@ -1,17 +1,18 @@
 package org.siemac.metamac.statistical.operations.core.serviceapi;
 
 import org.fornax.cartridges.sculptor.framework.test.AbstractDbUnitJpaTests;
-import org.junit.Test;
 
 /**
  * Spring based transactional test with DbUnit support.
  */
 public class StreamMessagingServiceTest extends AbstractDbUnitJpaTests implements StreamMessagingServiceTestBase {
-    @Test
+    @Override
     public void testSendMessage() {
+        // NO TEST
     }
 
-    @Test
+    @Override
     public void testResendAllPendingAndFailedMessages() {
+        // NO TEST
     }
 }
